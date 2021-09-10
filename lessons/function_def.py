5738ff8016f50ed23b41ed4f8b93cc56b714c202
Sydney Hardee <sydgray@email.unc.edu>
2021-09-09 22:00:57 -0400

    .

--- lessons/function_def.py
+++ lessons/function_def.py
@@ -7,6 +7,7 @@ def my_max(a: int, b: int) -> int:
         return a
     else:
         return b 
-print(my_max(10 + 1, 10))
-result: int = my_max(-50, 100)
-print(result)
+x: int = 6
+y: int = 5 + 2
+z: int = my_max(x, y)
+print(z)
