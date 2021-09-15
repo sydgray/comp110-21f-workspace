920c22961a456978b12a1396638f5323acdab8c6
Sydney Hardee <sydgray@email.unc.edu>
2021-09-15 16:22:28 -0400

    Progress on Exercise 3

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,21 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730400246"
+
+
+word: str = (input("Enter a word: "))
+dup: str = "Found duplicate: True"
+dup2: str = "Found duplicate: False"
+i: int = 0
+while i < len(word):
+    j: int = i + 1
+    while j < len(word):
+        char = word[j]
+        if char == word[i]:
+            print(dup)
+        else:
+            if i == 1:
+                print(dup2)
+        j = j + 1
+    i = i + 1
+    
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,15 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "751000940"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+depth: int = int(input("Depth: "))
+result: str = ""
+i: int = 0
+while i < depth:
+    if depth > 0:
+        print(TREE + result)
+        result = result + TREE
+    depth = depth - 1
+depth = depth - 1
